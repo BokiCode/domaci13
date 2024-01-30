00415a750094125a7045006e3a3f1803ba007370
BokiCode <bojancodedev@gmail.com>
2024-01-30 18:47:43 +0100

    imported jquery ui

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
+
     $("#clickButton").click(function () {
-        
+        console.log("test");
     });
+
+    $(".anchorLink").click(function () {
+        $(this).css({
+            backgroundColor: "red",
+            color: "white"
+        });
+    });
+    $("#hideParagraph").click(function () {
+        $("#paragraph").slideToggle();
+    });
+
 });
