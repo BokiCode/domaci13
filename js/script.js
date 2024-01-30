ae65eb0aabbeec662fa4de76d1561ef04c8d0bed
BokiCode <bojancodedev@gmail.com>
2024-01-30 20:33:47 +0100

    everything works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
-
+    $("#addButton").click(function () {
+        let num1 = parseInt($("#num1").val());
+        let num2 = parseInt($("#num2").val());
+        $("#resultHolder").html(num1 + num2);
+    });
+    $("#multiplyButton").click(function () {
+        let num1 = parseInt($("#num1").val());
+        let num2 = parseInt($("#num2").val());
+        if (num1 !== 0 && num2 !== 0) {
+            $("#resultHolder").html(num1 * num2);
+        } else {
+            $("#resultHolder").html("Ne mozete mnoziti sa nulom!");
+        }
+    });
+    $("#divideButton").click(function () {
+        let num1 = parseInt($("#num1").val());
+        let num2 = parseInt($("#num2").val());
+        if (num2 !== 0) {
+            $("#resultHolder").html(num1 / num2);
+        } else {
+            $("#resultHolder").html("Ne mozete deliti sa nulom!");
+        }
+    });
+    $("#subtractButton").click(function () {
+        let num1 = parseInt($("#num1").val());
+        let num2 = parseInt($("#num2").val());
+        $("#resultHolder").html(num1 - num2);
+    });
 });
