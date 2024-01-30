4807a3309e7db5573e4509a92a6b7e475264685b
BokiCode <bojancodedev@gmail.com>
2024-01-30 19:39:10 +0100

    second exercise

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 $(document).ready(function () {
+    //
+    // $("#clickButton").click(function () {
+    //     console.log("test");
+    // });
+    //
+    // $(".anchorLink").click(function () {
+    //     $(this).animate({
+    //         backgroundColor: "red",
+    //         color: "white"
+    //     }, 2000);
+    // });
+    // $("#hideParagraph").click(function () {
+    //     $("#paragraph").slideToggle();
+    // });
 
-    $("#clickButton").click(function () {
-        console.log("test");
-    });
-
-    $(".anchorLink").click(function () {
-        $(this).css({
-            backgroundColor: "red",
-            color: "white"
-        });
-    });
-    $("#hideParagraph").click(function () {
-        $("#paragraph").slideToggle();
+    $("#hideTextButton").click(function () {
+        let currentText = $(this).text();
+        if (currentText === "Hide Text") {
+            $(this).text("Show Text");
+        } else {
+            $(this).text("Hide Text");
+        }
+        $(".readLess").toggle();
     });
 
 });
